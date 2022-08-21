72dca00cedd2ac3302164aaa81677fb34b41cf17
Onyenokwe Chukwuka Great <chukwukaonyenokwe@gmail.com>
2022-08-21 23:59:24 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "sans-serif",
+        lora: "'Lora:ital', sans-serif",
+      },
+    },
+  },
+  plugins: []
+}
